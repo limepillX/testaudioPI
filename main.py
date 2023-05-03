b97adf370d1584a7ab826f70ea71cc3d8f8da997
tamu <justtedyt@gmail.com>
2023-05-03 10:39:58 +0400

    logger, mp3

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 import time
+from loguru import logger
 
 import RPi.GPIO as GPIO
 import os
 
-path_to_file = os.path.abspath('sound.m4a')
-
+path_to_file = os.path.abspath('sound.mp3')
+logger.debug(path_to_file)
 # Установка номера GPIO-пина для управления реле
 relay_pin = 23
-
+logger.info(f'Используем пин под номером {relay_pin}')
 # Инициализация библиотеки RPi.GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(relay_pin, GPIO.OUT)
+logger.info('GPIO initialized')
 
 # Включение реле
 GPIO.output(relay_pin, GPIO.HIGH)
+logger.info('rele turned ON')
 
 # Воспроизведение аудиофайла с помощью mpg321
 os.system(f'mpg321 {path_to_file}')
+logger.info('audio file started')
 
 time.sleep(3)
+logger.info('file finished')
 
 # Отключение реле
 GPIO.output(relay_pin, GPIO.LOW)
+logger.info('rele turned OFF')
 
 # Выключение библиотеки RPi.GPIO
 GPIO.cleanup()
+logger.info('cleaning up')
